e954ae159c43bc0e59de307d231cbf3a5896117c
L1keMagic <karachev.artur@gmail.com>
2021-10-18 21:56:20 +0300

    динамическая генерация компонентов

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 
+function Food({name}) {
+  return(<h3>я люблю {name}</h3>);
+}
+
+const foodILike = [
+  {
+    'name': 'борщ',
+    'image': 'https://i.imgur.com/afGMuQw.jpeg'
+  },
+  {
+    'name': 'уха',
+    'image': 'https://i.imgur.com/R1ti0sh_d.webp?maxwidth=760&fidelity=grand'
+  },
+  {
+    'name': 'щи',
+    'image': 'https://i.imgur.com/afGMuQw.jpeg'
+  },
+  {
+    'name': 'перец',
+    'image': 'https://i.imgur.com/afGMuQw.jpeg'
+  }
+]
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      {foodILike.map(dish => <Food name={dish.name} />)}
     </div>
   );
 }
