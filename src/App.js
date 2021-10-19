9838d324bcd15ab6aa9cf659ace3bf29d099de5f
L1keMagic <karachev.artur@gmail.com>
2021-10-19 13:18:57 +0300

    состояния

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({name, picture, raiting}) {
-  return(
-    <div>
-      <h3>я люблю {name}</h3>
-      <h4>{raiting}/5</h4>
-      <img src={picture} alt="" height='300px' />
-    </div>
-  );
-}
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  raiting: PropTypes.number.isRequired
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-const foodILike = [
-  {
-    'id': 1,
-    'name': 'борщ',
-    'image': 'https://i.imgur.com/afGMuQw.jpeg',
-    'raiting': 4.1
-  },
-  {
-    'id': 2,
-    'name': 'уха',
-    'image': 'https://i.imgur.com/R1ti0sh_d.webp?maxwidth=760&fidelity=grand',
-    'raiting': 4.4
-  },
-  {
-    'id': 3,
-    'name': 'щи',
-    'image': 'https://i.imgur.com/afGMuQw.jpeg',
-    'raiting': 4.3
-  },
-  {
-    'id': 4,
-    'name': 'перец',
-    'image': 'https://i.imgur.com/afGMuQw.jpeg',
-    'raiting': 5.0
+  plus = () => {
+    this.setState(current => ({count: current.count + 1}))
   }
-]
+  minus = () => {
+    this.setState(current => ({count: this.state.count - 1}))
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} raiting={dish.raiting} />)}
+  render() {
+    return <div>
+     <h1>Current number: {this.state.count}</h1>
+     <button onClick={this.plus}>Плюс</button>
+     <button onClick={this.minus}>Минус</button>
     </div>
-  );
+  }
 }
 
 export default App;
